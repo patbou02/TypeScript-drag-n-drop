a15f995a5451c2da6b24181f588ddfc153808b1d
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-04-15 15:35:39 -0700

    Refactored how each project item gets generated so that it uses a newly created ProjectItem class which extends the Component base class. Updated template to output more information inside h2, h3 and p elements.

--- src/app.ts
+++ src/app.ts
@@ -89,11 +89,7 @@ function validate(validatableInput: Validatable) {
 }
 
 // autobind decorator
-function Autobind(
-  _: any,
-  _2: string,
-  descriptor: PropertyDescriptor
-) {
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
@@ -135,6 +131,27 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement , HTMLLIElement>{
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -170,9 +187,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.querySelector(`#${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = '';
     for (const projItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = projItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, projItem);
     }
   }
 }
