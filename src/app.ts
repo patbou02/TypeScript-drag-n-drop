2f08f943bcc61659bd884e4a0907b4259d23ce29
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-04-16 01:39:33 -0700

    Updated drop handler by creating moveProject method inside ProjectState class which we pass a project id and a new status and use that to determine if we need to re-render the list if the project dropped has a different status than new status.

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,18 @@ class ProjectState extends State<Project> {
       ProjectStatus.Active
   );
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(proj => proj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -206,8 +218,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
   }
 
+  @Autobind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const projId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(projId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
   }
 
   @Autobind
