027d818e29f68b70872ebf29ca37d0385ce75312
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-04-16 00:48:29 -0700

    Made li elements draggable. Also created 2 interfaces for draggable and dragtarget and set the ProjectItem class to implement the Draggable interface before setting start/end handlers for dragging.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 // Code goes here!
 console.log('Running App.js...');
 
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 enum ProjectStatus { Active, Finished}
 
 // Project Type
@@ -132,7 +144,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement , HTMLLIElement>{
+class ProjectItem extends Component<HTMLUListElement , HTMLLIElement> implements Draggable {
   private project: Project;
 
   get people() {
@@ -151,7 +163,19 @@ class ProjectItem extends Component<HTMLUListElement , HTMLLIElement>{
     this.renderContent();
   }
 
-  configure() {}
+  @Autobind
+  dragStartHandler(event: DragEvent) {
+    console.log(event);
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd');
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
 
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
