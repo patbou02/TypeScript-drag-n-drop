d7dadad348687a9d8af7ae86fa15b033d4427a2e
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-04-16 01:23:33 -0700

    Created droppable area by updating dargStart and dropOver handlers. set data within data transfer along with ID being passed when dropped.

--- src/app.ts
+++ src/app.ts
@@ -165,7 +165,8 @@ class ProjectItem extends Component<HTMLUListElement , HTMLLIElement> implements
 
   @Autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
   }
 
   dragEndHandler(_: DragEvent) {
@@ -197,19 +198,20 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
   }
 
   @Autobind
-  dragOverHandler(_: DragEvent) {
-    console.log('DragOver');
-    const listEl = this.element.querySelector('ul')!;
-    listEl.classList.add('droppable');
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+      event.preventDefault();
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+    }
   }
 
-  dropHandler(_: DragEvent) {
-    //console.log('drop');
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData('text/plain'));
   }
 
   @Autobind
   dragLeaveHandler(_: DragEvent) {
-    //console.log('DragLeave');
     const listEl = this.element.querySelector('ul')!;
     listEl.classList.remove('droppable');
   }
